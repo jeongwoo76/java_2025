dab482dbce61c5816b9dbc6f6e6634cd95543b6a
jeongwoo76 <jeongwoo3701@naver.com>
2025-02-18 18:12:36 +0900

    2025.02.18 두번째날 자바

---
+++ java_basic/src/com/company/java002_ex/PrintEx001.java
@@ -0,0 +1,17 @@
+package com.company.java002_ex;
+
+public class PrintEx001 {
+	public static void main(String[] args) {
+		// printf %s
+		System.out.printf("좋아하는 색상은 %s입니다", "RED");
+	}
+}
+/*
+ * 연습문제1)  
+패키지명 : com.company.java002_ex
+클래스명 : PrintEx001
+출력내용 : 
+   %s를 이용해서  다음과 같이 출력
+   
+    좋아하는 색상은 RED입니다.
+ */
