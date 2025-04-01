039962d7d7eabecc858c498cea050672e9977e03
jeongwoo76 <jeongwoo3701@naver.com>
2025-04-01 18:30:12 +0900

    2025.04.01 spring002 datasource + mybatis board 테스트

--- mysql_basic/mysql008_board.sql
+++ mysql_basic/mysql008_board.sql
@@ -9,4 +9,14 @@ bdate timestamp not null default current_timestamp,
 bip	varchar(100) not null );
 desc board;
 
-
+create table sboard (
+bno int not null auto_increment primary key,
+bname varchar(100) not null,
+bpass varchar(50) not null,
+btitle varchar(1000) not null,
+bcontent text not null,
+bdate timestamp not null default current_timestamp,
+bhit int not null default 0,
+bip	varchar(50) not null );
+desc sboard;
+alter table sboard modify bhit int not null default 0;
