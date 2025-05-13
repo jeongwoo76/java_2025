ed5a608ff47b24ed46ac410e30b7af22a0476ad7
jeongwoo <jeongwoo3701@naver.com>
2025-05-13 18:06:19 +0900

    2025. 05. 13 node.js 2 db sequelize  3. model - 시퀄라이즈 모델 / 관계

--- node_react2025/back/app.js
+++ node_react2025/back/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app     = express();
-const basicRouter = require('./routes/basic'); //## 추가1
-const testRouter = require('./routes/test'); //## 추가1
+const basicRouter = require('./routes/basic'); 
+const testRouter = require('./routes/test'); 
+
+///////////////// Models
+const db = require('./models');
+db.sequelize
+  .sync()
+  .then(() => {console.log('db.....'); })
+  .catch( console.error );
 
 
-//1. 경로 /
+///////////////// Router
 app.get('/', (req, res) => { res.send('Hello express'); });
-
-app.use('/api', basicRouter); //## 추가2
-app.use('/test', testRouter); //## 추가2
+app.use('/api', basicRouter); 
+app.use('/test', testRouter); 
 
 
 
